d0ce716393eff32c278becfdd40938fe2d35bcce
Sally Young <sally@justafish.co.uk>
2013-09-13 15:49:33 +0100

    Add services for retrieving site settings, node information, menus and template partial paths.

--- angular/app/js/services.js
+++ angular/app/js/services.js
@@ -1,4 +1,70 @@
 'use strict';
 
-angular.module('myApp.services', [])
+angular.module('myApp.services', ['ngResource'])
+
+  .value('cms', 'http://cms.londonlindyexchange.com')
+
+  .factory('cmsSiteSettings', ['$resource', 'cms', function($resource, cms) {
+    return $resource(cms + '/api/1.0/site-information.json', {}, {
+      get: {
+        method: 'GET',
+        cache: true
+      }
+    });
+  }])
+
+  .factory('cmsMainMenu', ['$resource', 'cms', function($resource, cms) {
+    return $resource(cms + '/api/1.0/main-menu.json', {}, {
+      get: {
+        method: 'GET',
+        cache: true
+      }
+    });
+  }])
+
+  .factory('cmsNode', ['$resource', 'cms', function($resource, cms) {
+    return $resource(cms + '/api/1.0/nodes.json',
+      {
+        path: '@path'
+      },
+      {
+        get: {
+          method: 'GET',
+          cache: true
+        }
+      }
+    );
+  }])
+
+  .factory('cmsTemplate', ['$http', '$templateCache', '$q', '$timeout', function($http, $templateCache, $q, $timeout) {
+    return {
+      getPartial: function(node) {
+        var deferred = $q.defer();
+        $timeout(function() {
+          var templateUrl = '/_partials/nodes/' + node.type + '.html';
+          $http.get(templateUrl, {
+            cache: $templateCache
+          })
+            .success(function(data) {
+              $templateCache.put(templateUrl, data);
+              deferred.resolve(templateUrl);
+            })
+            .error(function(data) {
+              var genericTemplateUrl = '/_partials/nodes/node.html';
+              $http.get(genericTemplateUrl, {
+                cache: $templateCache
+              })
+                .success(function(data) {
+                  $templateCache.put(templateUrl, data);
+                  deferred.resolve(templateUrl);
+                })
+                .error(function(data){
+                  deferred.reject();
+                });
+          });
+        });
+        return deferred.promise;
+      }
+    };
+  }])
 ;
